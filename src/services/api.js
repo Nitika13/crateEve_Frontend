a96e03ca99b952f6b995b84bd1c51e50cc7f3a9c
Nitika13 <ahlawatnitika4@gmail.com>
2025-06-29 19:49:56 +0530

    Initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: "http://localhost:8080/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");

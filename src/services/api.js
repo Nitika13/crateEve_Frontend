c2a22595f5df114ab279ecf8828d4bb2d9821eb0
Nitika13 <ahlawatnitika4@gmail.com>
2025-06-30 19:25:28 +0530

    Updated

--- src/services/api.js
+++ src/services/api.js
@@ -19,26 +19,7 @@
 
 // export default api;
 
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_BACKEND_URL,
-//   headers: {
-//     "Content-Type": "application/json"
-//   }
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default api;
-
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
@@ -47,17 +28,12 @@ const api = axios.create({
   }
 });
 
-// Automatically add token before each request
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
-
